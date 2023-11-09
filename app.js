26f95ce92fecd4113b22ef37d512cc833c524648
Eyasu Araya <128852829+EyasuTes@users.noreply.github.com>
2023-11-09 08:05:30 +0400

    added the cors for the

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
     origin: "http://localhost:5173",
+    origin: "https://messageapp-3-backend1.onrender.com",
     // credentials: true,
   },
 });
