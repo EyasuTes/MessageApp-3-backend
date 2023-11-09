93c92ef049c048afe2a6d68745f36b3e1f327fc8
Eyasu Araya <128852829+EyasuTes@users.noreply.github.com>
2023-11-09 08:07:13 +0400

    added the cors for the frontend

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
     origin: "http://localhost:5173",
-    origin: "https://messageapp-3-backend1.onrender.com",
+    origin: "https://message-app-3-frontend.vercel.app/",
     // credentials: true,
   },
 });
