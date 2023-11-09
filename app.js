3eb1897882ab9b9fb9978857fb86bdf6972b3549
Eyasu Araya <128852829+EyasuTes@users.noreply.github.com>
2023-11-09 07:36:19 +0400

    removed the data import form app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const chats = require("./data/data.js");
+
 const dotenv = require("dotenv");
 const mongodbConnect = require("./config/db.js");
 const userRoutes = require("./routes/userRoutes");
