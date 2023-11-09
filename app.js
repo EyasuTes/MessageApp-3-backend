52476c08e62f3a6df8130fc036edd298f4f86426
Eyasu Araya <128852829+EyasuTes@users.noreply.github.com>
2023-11-09 08:32:37 +0400

    modified cors

--- app.js
+++ app.js
@@ -29,8 +29,11 @@ const server = app.listen(3001);
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:5173",
-    origin: "https://message-app-3-frontend.vercel.app/",
+    origin: [
+      "http://localhost:5173",
+      "https://message-app-3-frontend.vercel.app",
+    ],
+
     // credentials: true,
   },
 });
